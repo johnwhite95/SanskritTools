efda028816eab2b4d9777e562f0fccd212672f6f
John White <jbenjaminw@gmail.com>
2018-05-09 21:31:27 -0400

    Add files via upload

--- ALTkit.py
+++ ALTkit.py
@@ -1,40 +1,124 @@
-import pandas as pd
-pairs11 = [
-           ["a", "a"], ["a", "i"], ["i", "ī"], ["i", "a"], 
-           ["h", "s"], ["u", "u"], ["m", "s"], ["t", "j"],
-           ["ṭ", "h"], ["n", "t"], ["a", "ḥ"], ["a", "aḥ"],
-           ["a", "an"]
-          ]
-joins11 = [
-           "ā", "e", "ī", "y a", 
-           "kṣ", "ū", "ṃs", "j j",
-           "ḍ ḍh", "ṃs t", "aḥ", "āḥ",
-           "ān"
-          ]
-pairs21 = [
-           ["au", "ā"]
-          ]
-joins21 = [
-           "āv ā"
-          ]
-pairs22 = [
-           ["dh", "dh"]
-          ]
-joins22 = [
-           "ddh"
-          ]
 
-cases = ["Nom.", "Voc.", "Acc.", "Ins.", "Dat.", "Abl.", "Gen.", "Loc."]
+# coding: utf-8
+
+# In[482]:
+
+
+import pandas as pd
+
+D_vowel = [
+    "अ", "आ", "इ", "ई", "उ", "ऊ", "ए", "ऐ", "ओ",
+    "औ", "ऋ", "ॠ", "ऌ", "ॡ", "ं", "ः"
+]
+R_vowel = [
+    "a", "ā", "i", "ī", "u", "ū", "e", "ai", "o",
+    "au", "ṛ", "ṝ", "ḷ", "ḹ", "aṃ", "ḥ"
+]
+
+dia_d = [
+    " ", "ि", "ु", "ॢ"
+]
+
+dia_r = [
+    "a", "i", "u", "ḷ"
+]
+
+D_cons = [
+    "क", "ख", "ग", "घ", "ङ", "च", "छ", "ज", "झ",
+    "ञ", "ट", "ठ", "ड", "ढ", "ण", "त", "थ", "द",
+    "ध", "न", "प", "फ", "ब", "भ", "म", "य", "र",
+    "ल", "व", "श", "ष", "स", "ह"
+]
+"""
+R_cons = [
+    "k", "kh", "g", "gh", "ṅ", "c", "ch",
+    "j", "jh", "ñ", "ṭ", "ṭh", "ḍ", "ḍh",
+    "ṇ", "t", "th", "d", "dh", "n", "p",
+    "ph", "b", "bh", "m", "y", "r", "l",
+    "v", "ś", "ṣ", "s", "h"
+]"""
+
+R_cons = [
+    "ka", "kha", "ga", "gha", "ṅa", "ca", "cha",
+    "ja", "jha", "ña", "ṭa", "ṭha", "ḍa", "ḍha",
+    "ṇa", "ta", "tha", "da", "dha", "na", "pa",
+    "pha", "ba", "bha", "ma", "ya", "ra", "la",
+    "va", "śa", "ṣa", "sa", "ha"
+]
+
+pairs11 = [
+    ["a", "a"], ["a", "i"], ["i", "ī"], ["i", "a"], 
+    ["h", "s"], ["u", "u"], ["m", "s"], ["t", "j"],
+    ["ṭ", "h"], ["n", "t"], ["a", "ḥ"], ["a", "aḥ"],
+    ["a", "an"]
+]
+
+joins11 = [
+    "ā", "e", "ī", "y a", 
+    "kṣ", "ū", "ṃs", "j j",
+    "ḍ ḍh", "ṃs t", "aḥ", "āḥ",
+    "ān"
+]
+
+pairs21 = [
+    ["au", "ā"]
+]
+
+joins21 = [
+    "āv ā"
+]
+
+pairs22 = [
+    ["dh", "dh"]
+]
+joins22 = [
+    "ddh"
+]
+
+cases = ["Nom.", "Voc.", "Acc.", "Ins.", "Dat.", "Abl.", 
+         "Gen.", "Loc."]
+
 singdec_ma = ["aḥ", "a", "am", "ena", "āya", "āt", "asya", "e"]
 dualdec_ma = ["au", "au", "au", "ābhyām", "ābhyām", "ābhyām", "ayoḥ", "ayoḥ"]
-plurdec_ma = ["āḥ", "āḥ", "ān", "aiḥ", "ebhyaḥ", "ebhyaḥ", "ānām", "eṣu"]
-
+plurdec_ma = ["āḥ", "āḥ", "ān", "aiḥ", "ebhyaḥ", "ebhyaḥ", 
+              "ānām", "eṣu"]
 singdec_na = ["am", "a", "am", "ena", "āya", "āt", "asya", "e"]
-dualdec_na = ["e", "e", "e", "ābhyām", "ābhyām", "ābhyām", "ayoḥ", "ayoḥ"]
-plurdec_na = ["āni", "āni", "āni", "aiḥ", "ebyaḥ", "ebyaḥ", "ānām", "eṣu"]
+dualdec_na = ["e", "e", "e", "ābhyām", "ābhyām", "ābhyām", 
+              "ayoḥ", "ayoḥ"]
+plurdec_na = ["āni", "āni", "āni", "aiḥ", "ebyaḥ", "ebyaḥ", 
+              "ānām", "eṣu"]
+
+singdec_mi = ["is", "e", "im", "inā", "aye", "es", "es", "au"]
+dualdec_mi = ["ī", "ī", "ī", "ibhyām", "ibhyām", "ibhyām", 
+              "yos", "yos"]
+plurdec_mi = ["ayas", "ayas", "īn", "ibhis", "ibhyas", 
+              "ibhyas", "īnām", "iṣu"]
+singdec_ni = ["i", "i/-e", "i", "iṇā", "iṇe", "iṇas", "iṇas", "iṇi"]
+dualdec_ni = ["iṇī", "iṇī", "iṇī", "ibhyām", "ibhyām", 
+              "ibhyām", "iṇnos", "iṇnos"]
+plurdec_ni = ["īṇī", "īṇī", "īṇī", "ibhis", "ibhyas", 
+              "ibhyas", "īṇām", "iṣu"]
+singdec_fi = ["is", "e", "im", "yā", "aye", "es", "es", "au"]
+dualdec_fi = ["ī", "i", "ī", "ibhyām", "ibhyām", "ibhyām", 
+              "yos", "yos"]
+plurdec_fi = ["ayas", "aya", "is", "ibhis", "ibhyas", 
+              "ibhyas", "īnām", "iṣu"]
+
+ma_dec = [singdec_ma, dualdec_ma, plurdec_ma]
+na_dec = [singdec_na, dualdec_na, plurdec_na]
+mi_dec = [singdec_mi, dualdec_mi, plurdec_mi]
+ni_dec = [singdec_ni, dualdec_ni, plurdec_ni]
+fi_dec = [singdec_fi, dualdec_fi, plurdec_fi]
+
+relationship_nouns = ["pitṛ", "matṛ"]
 
 persons = ["third", "second", "first"]
-pres_ten_end = ["ati", "asi", "āmi", "-", "-", "āvaḥ", "anti", "-", "āmaḥ"]
+pres_ten_end = ["ati", "asi", "āmi", "-", "-", "āvaḥ", 
+                "anti", "-", "āmaḥ"]
+
+
+# In[430]:
+
 
 def sandhi(x, y):
     check11 = [x[len(x)-1:], y[:1]]
@@ -59,29 +143,86 @@ def sandhi(x, y):
     else:
         return(x + y)
 
-def decline(noun, gender):
-    stem_cut = noun[:len(noun)-1]
-    stem = noun[len(noun)-1:]
+
+# In[431]:
+
+
+def list_declen(without_stem, declist):
     singular = []
     dual = []
     plural = []
-    if stem == "a":
-        if gender == "masc":
-            for i in range(0, len(singdec_ma)):
-                singular.append(sandhi(stem_cut, singdec_ma[i]))
-                dual.append(sandhi(stem_cut, dualdec_ma[i]))
-                plural.append(sandhi(stem_cut, plurdec_ma[i]))
-        elif gender == "neut":
-            for i in range(0, len(singdec_ma)):
-                singular.append(stem_cut + singdec_na[i])
-                dual.append(stem_cut + dualdec_na[i])
-                plural.append(stem_cut + plurdec_na[i])
-        finished = pd.DataFrame({'case':cases, 'singular':singular, 
-                                 'dual':dual, 'plural':plural}).reindex(
-                                ['case', 'singular', 'dual', 'plural'], axis=1)
+    for i in range(0, 8):
+        singular.append(without_stem + declist[0][i])
+        dual.append(without_stem + declist[1][i])
+        plural.append(without_stem + declist[2][i])
+    finished = pd.DataFrame({'case':cases, 'singular':singular, 
+                             'dual':dual, 'plural':plural}).reindex(
+                            ['case', 'singular', 'dual', 'plural'], axis=1)  
     return(finished)
 
-print(decline("vacana", "neut"))
+
+# In[432]:
+
+
+def decline(noun, gender):
+    stem_cut = noun[:len(noun)-1]
+    stem = noun[len(noun)-1:]
+    if stem == "a":
+        if gender == "masc":
+            output = list_declen(stem_cut, ma_dec)
+        elif gender == "neut":
+            output = list_declen(stem_cut, na_dec)
+    elif stem == "i":
+        if gender == "masc":
+            output = list_declen(stem_cut, mi_dec)
+        elif gender == "neut":
+            output = list_declen(stem_cut, ni_dec)
+        elif gender == "fem":
+            output = list_declen(stem_cut, fi_dec)
+    elif stem == "ṛ":
+        output = "blah"
+    else:
+        output = "noun not recognized"
+    return(output)
+
+
+# In[433]:
+
+
+decline("phala", "neut")
+
+
+# In[434]:
+
+
+def pronouns(gender):
+    persons = ["third", "second", "first", "interr"]
+    if gender == "masc":
+        first = ['sa/saḥ', 'tvam', 'aham', 'kaḥ']
+        second = ['tam', 'tvām', 'mām', 'kam']
+        third = ['tam', 'tvām', 'mām', 'kam']
+        finished_sing = pd.DataFrame({'person':persons, 'nom':first, 'voc':second,
+                                'acc':third}).reindex(
+                                ['person', 'nom', 'voc', 'acc'], axis=1)
+    elif gender == "neut":
+        first = ['tat', 'tvam', 'aham', 'kim']
+        second = ['tat', 'tvām', 'mām', 'kim']
+        third = ['tat', 'tvām', 'mām', 'kim']
+        finished_sing = pd.DataFrame({'person':persons, 'nom':first, 'voc':second,
+                                'acc':third}).reindex(
+                                ['person', 'nom', 'voc', 'acc'], axis=1)  
+        
+    return(finished_sing)
+
+
+# In[435]:
+
+
+pronouns("neut")
+
+
+# In[436]:
+
 
 def conjugate(verb):
     verb_cut = verb[:len(verb)-1]
@@ -102,3 +243,49 @@ def conjugate(verb):
 conjugate("gaccha")
 
 
+# In[437]:
+
+
+"""maybe include an option for clusters"""
+def devanagari(kind):
+    if kind == "vowels":
+        finished_sing = pd.DataFrame({'D_vowel':D_vowel, 'R_vowel':R_vowel}).reindex(
+                                ['D_vowel', 'R_vowel'], axis=1)
+        return(finished_sing)
+
+    elif kind == "consonants":
+        finished_sing = pd.DataFrame({'D_cons':D_cons, 'R_cons':R_cons}).reindex(
+                                ['D_cons', 'R_cons'], axis=1)
+        return(finished_sing)
+    else:
+        return("error: invalid argument")
+
+
+# In[438]:
+
+
+devanagari("vowels")
+
+
+# In[483]:
+
+
+def romanize(text):
+	out1 = []
+	out2 = []
+	out_a = []
+	dev = D_vowel + D_cons + dia_d
+	rom = R_vowel + R_cons + dia_r
+	for i in range(0, len(text)):
+		character = text[i]
+		phon = rom[dev.index(text[i])]
+		if character in dia_d:
+			out1[i-1] = (out1[i-1][:len(out1[i-1])-1] + phon)
+			out2.append(character)
+		else:
+			out1.append(phon)
+			out2.append(character)
+	return(''.join(str(x) for x in out1))
+
+
+print(romanize("पपि"))
